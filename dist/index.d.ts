0f1962566a65b7c9a1b74d5249f7fcf7a9c12ffd
andrewangelle <andrewangelle@gmail.com>
2019-05-27 16:21:23 -0500

    latest

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,54 @@
 import '@babel/polyfill';
 import React from 'react';
-import { PayPalButtonProps, PayPalPaymentData } from './types';
+/**
+ * types
+ */
 interface State {
     loaded: boolean;
     error: boolean;
 }
-declare class PayPalButton extends React.Component<PayPalButtonProps, State> {
+export declare type PayPalPaymentData = {
+    cart: string;
+    create_time: string;
+    id: string;
+    intent: 'sale' | 'purchase';
+    payer: {
+        payer_info: {
+            country_code: string;
+            email: string;
+            first_name: string;
+            last_name: string;
+            middle_name: string;
+            payer_id: string;
+            shipping_address: {
+                city: string;
+                country_code: string;
+                line1: string;
+                postal_code: string;
+                recipient_name: string;
+                state: string;
+            };
+        };
+        payment_method: string;
+        status: 'UNVERIFIED' | 'VERIFIED';
+    };
+    state: string;
+    transaction: any[];
+};
+export declare type PayPalButtonProps = {
+    env: 'sandbox' | 'production';
+    sandboxID?: string;
+    productionID?: string;
+    amount: number;
+    currency: string;
+    onPaymentError?: (msg: string) => void;
+    onPaymentStart?: () => void;
+    onPaymentSuccess?: (response: PayPalPaymentData) => void;
+};
+/**
+ * component
+ */
+export declare class PayPalButton extends React.Component<PayPalButtonProps, State> {
     constructor(props: PayPalButtonProps);
     componentDidMount(): Promise<void>;
     componentDidCatch(): void;
@@ -13,4 +56,4 @@ declare class PayPalButton extends React.Component<PayPalButtonProps, State> {
     onAuthorize(data: any, actions: any): void;
     render(): false | JSX.Element | null;
 }
-export { PayPalButton, PayPalButtonProps, PayPalPaymentData };
+export {};
