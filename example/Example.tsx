a5c74077809895c2aaef106ae2f7d2666747b580
andrewangelle <andrewangelle@gmail.com>
2019-08-22 14:42:52 -0500

    remove dependency on paypal checkout lib

--- example/Example.tsx
+++ example/Example.tsx
@@ -15,18 +15,6 @@ export function Example() {
   return (
     <div style={buttonStyles as any}>
       <h3>Try me out</h3>
-      {/* <PayPalButton
-        env="sandbox"
-        amount={1.00}
-        sandboxID="12345" // <- replace with an actual id
-        currency="USD"
-        onPaymentStart={() => console.log('payment started')}
-        onPaymentSuccess={(res: PayPalPaymentData) => console.log('payment complete', res)}
-        onPaymentError={(msg: string) => console.log('payment error', msg)}
-        onPaymentCancel={(data: OnCancelData) => console.log(data)}
-        onShippingChange={(data: OnShippingChangeData) => console.log('onShippingChange', data)}
-      /> */}
-
       <PayPalButton
         env="sandbox"
         amount={1.00}
