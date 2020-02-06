7a97d6a1c8e9cdb4a48f1a1cbd49ee2de92c369b
Josh Unwin <joshunwin@protonmail.com>
2020-02-06 20:05:04 +0000

    removed a commented out line

--- example/ExampleWithPriceChange.tsx
+++ example/ExampleWithPriceChange.tsx
@@ -9,7 +9,6 @@ const wrapperStyles: React.CSSProperties = {
 }
 
 const paypalOptions: PaypalOptions = {
-  // clientId: 'process.env.PAYPAL_CLIENT_ID!',
   clientId: process.env.PAYPAL_CLIENT_ID!,
   intent:'capture',
   currency:'USD',
